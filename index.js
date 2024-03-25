d1a329462e640ec3935ca1c654e5e91b2dc71b99
Naincy <naincyk15042004@gmail.com>
2024-03-25 18:48:50 +0530

    Added toggle function in header

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
-    $(".menu").click(function(){
-        $(".navleft").toggleClass("open"); // Toggle the 'open' class on click
-    });
+document.getElementById("menuButton").addEventListener("click", function() {
+    var menuContent = document.getElementById("menuContent");
+    menuContent.style.display = (menuContent.style.display === "none") ? "block" : "none";
+    
+    var menuButton = document.getElementById("menuButton");
+    if (menuContent.style.display === "block") {
+        menuButton.innerHTML = '<img style="width: 1.5rem; height: 1rem;" src="image2/cross.png" alt="Close Icon">&nbsp;&nbsp;CLOSE';
+    } else {
+        menuButton.innerHTML = '<img style="width: 1.5rem; height: 1rem;" src="image2/menu2.png" alt="Menu Icon">&nbsp;&nbsp;MENU';
+    }
 });
